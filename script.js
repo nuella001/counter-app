efdc6c095e6e82aa9bfe32b71d131d3b8c8ee0f9
nuella001 <chiomamemmanuella@gmail.com>
2022-09-07 21:42:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// const add = document.querySelector('.add');
+// const sub = document.querySelector('.subtract');
+// const resetCount = document.querySelector('.reset');
+const count = document.querySelector('.count');
+const buttons = document.querySelector ('.buttons');
+
+
+
+buttons.addEventListener('click', (e) => {
+    if (e.target.classList.contains('add')) {
+        count.innerHTML++
+        setColor()
+    }
+    if (e.target.classList.contains('subtract')) {
+        count.innerHTML--
+        setColor()
+    }
+    if (e.target.classList.contains('reset')) {
+        count.innerHTML = 0
+        setColor()
+    }
+})
+  function setColor() {
+    if (count.innerHTML > 0){
+        count.style.color =' green'
+    }
+    else if (count.innerHTML < 0) {
+        count.style.color ='red'
+    }
+    else{count.style.color ='white'}
+  }
+
+
+// add.addEventListener('click', () => {
+//     count.innerHTML++
+// })
+// sub.addEventListener('click', () => {
+//     count.innerHTML--
+// })
+// resetCount.addEventListener('click', () => {
+//     count.innerHTML = 0
+// })
